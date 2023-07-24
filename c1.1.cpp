756a0cf7c1c58af7429ddcb658e3a01725b38c6d
Jimzical <arishkumar2003@gmail.com>
2023-07-24 09:56:43 +0530

    first commit

---
+++ c1.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// int new(){
+//     int i;
+//     std::cin << i;
+//     std::cout << i << std::endl;
+//     return 0;
+// }
+
+int main(void){
+    std::cout<<"HII";
+    return 0;
+}
