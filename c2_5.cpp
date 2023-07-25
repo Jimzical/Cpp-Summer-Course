9e1dfeb162cf714c96ce5c9e12fd6ecc3690c841
Jimzical <arishkumar2003@gmail.com>
2023-07-25 11:58:50 +0530

    moved files

---
+++ c2_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    int &t;
+    public:
+        Test(int &t):t(t){}
+        int getT(){return t;}
+};
+
+int main(int argc, char const *argv[]){
+    int x = 29;
+    Test t1(x);
+    cout << t1.getT() << endl;
+    x = 34;
+    cout << t1.getT() << endl;
+
+    return 0;
+}
