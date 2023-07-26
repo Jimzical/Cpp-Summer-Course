d2d66cb92c7696131ff0d1621c4dd881f213e71c
Jimzical <arishkumar2003@gmail.com>
2023-07-26 12:18:15 +0530

    NEW

--- c3_3_.cpp
+++ c3_3_.cpp
@@ -3,6 +3,25 @@
 #include <memory.h> 
 using namespace std;
 
+class Time{
+    private:
+        int hour;
+        int mins;
+
+    public:
+        Time(){
+            hour = 0;
+            mins = 0;
+        }
+
+        Time(int t){
+            cout << "Conversion happening" << endl;
+            hour = t / 60;
+            mins = t % 60;
+        }
+
+        
+};
 
 int main(int argc, char const *argv[]){
 
